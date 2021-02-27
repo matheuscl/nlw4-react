8c29151429dcf8e5682383a996505fb0ee1d2054
Matheus Lopes <matheus1994lopes@gmail.com>
2021-02-27 18:35:48 -0300

    Creating Countdown Context, ChallengeBox can now control countdown by context API and added browser notification and sound to notify user when time runs out

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from 'react'
+import { createContext, useState, ReactNode, useEffect } from 'react'
 import challenges from '../../challenges.json'
 
 interface IChallenge {
@@ -16,6 +16,7 @@ interface ChallengeContextData {
     levelUp: () => void,
     startNewChallenge: () => void,
     resetChallenge: () => void,
+    completeChallenge: () => void,
 }
 
 export const ChallengesContext = createContext({} as ChallengeContextData)
@@ -32,6 +33,10 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
 
+    useEffect(() => {
+        Notification.requestPermission()
+    }, [])
+
     function levelUp() {
         setLevel(level + 1)
     }
@@ -41,10 +46,37 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         const challenge = challenges[randomChallengeIndex]
 
         setActiveChallenge(challenge)
+
+        new Audio('/notification.mp3').play()
+
+        if(Notification.permission === 'granted') {
+            new Notification('Novo desafio parsa', {
+                body: `Valendo ${challenge.amount}`
+            })
+        }
     }
 
     function resetChallenge() {
         setActiveChallenge(null)
+    }
+
+    function completeChallenge() {
+        if(!activeChallenge) {
+            return;
+        }
+
+        const { amount } = activeChallenge
+
+        let finalExperience = currentExperience + amount
+
+        if(finalExperience >= experienceToNextLevel) {
+            finalExperience = finalExperience - experienceToNextLevel
+            levelUp()
+        }
+
+        setCurrentExperience(finalExperience)
+        setActiveChallenge(null)
+        setChallengesCompleted(challengesCompleted + 1)
     }
 
     return (
@@ -56,7 +88,8 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
             experienceToNextLevel,
             levelUp,
             startNewChallenge,
-            resetChallenge
+            resetChallenge,
+            completeChallenge
         }}>
             {children}
         </ChallengesContext.Provider>

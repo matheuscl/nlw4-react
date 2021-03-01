4521ef81e65b1f3245e01bad2762625297f8ba41
Matheus Lopes <matheus1994lopes@gmail.com>
2021-02-28 23:56:46 -0300

    Fixing indentation

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -5,11 +5,11 @@ import styles from '../styles/components/Countdown.module.css'
 export function Countdown() {
     const {
         minutes,
-            seconds,
-            hasFinished,
-            isActive,
-            startCountdown,
-            resetCountdown
+        seconds,
+        hasFinished,
+        isActive,
+        startCountdown,
+        resetCountdown
     } = useContext(CountdownContext)
 
     const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
